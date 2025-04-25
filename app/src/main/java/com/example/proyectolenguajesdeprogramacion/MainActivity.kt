43553d59cc6615a8fec02a272a125aa238ee192b
Yael Quintanilla <153146490+YaelQR@users.noreply.github.com>
2025-04-24 21:25:13 -0600

    Se agregaron las dependencias necesarias para que funcionaran los servicios de Firebase y se hizo una prueba en MainActivity

--- app/src/main/java/com/example/proyectolenguajesdeprogramacion/MainActivity.kt
+++ app/src/main/java/com/example/proyectolenguajesdeprogramacion/MainActivity.kt
@@ -7,8 +7,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.content.Intent
 import android.widget.Button
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,5 +31,23 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, LogininicialActivity::class.java)
             startActivity(intent)
         }
+
+        auth = FirebaseAuth.getInstance()
     }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        Toast.makeText(this, "Se verifico si el usuario estaba logueado. El usuario es: $currentUser", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "El usuario es: $currentUser", Toast.LENGTH_SHORT).show()
+        if (currentUser != null) {
+            reload()
+        }
+    }
+
+    fun reload() {
+        Toast.makeText(this, "Usuario no inicio sesión", Toast.LENGTH_SHORT).show()
+    }
+
 }
