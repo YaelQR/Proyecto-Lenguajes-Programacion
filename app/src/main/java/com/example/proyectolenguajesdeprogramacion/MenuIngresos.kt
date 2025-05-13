ad2314f649e59e683ab00e5a72f5c4bfb4ce7250
Bryan Ramirez <203480660+RagaTV@users.noreply.github.com>
2025-05-13 02:37:03 -0600

    se creo MenuIngresos.kt y se agrego un boton que conecta las clases agregarIngreso o Gasto

--- app/src/main/java/com/example/proyectolenguajesdeprogramacion/MenuIngresos.kt
+++ app/src/main/java/com/example/proyectolenguajesdeprogramacion/MenuIngresos.kt
@@ -8,22 +8,22 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MenuGastos : AppCompatActivity() {
+class MenuIngresos : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_gastos)
+        setContentView(R.layout.activity_menu_ingresos)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        val buttonAgregar = findViewById<Button>(R.id.AgregarGastoBtn)
+        val buttonAgregar = findViewById<Button>(R.id.AgregarIngresoBtn)
         buttonAgregar.setOnClickListener {
-            val intent = Intent(this, AgregarGastoActivity::class.java)
+            val intent = Intent(this, AgregarIngresoActivity::class.java)
             startActivity(intent)
         }
     }
